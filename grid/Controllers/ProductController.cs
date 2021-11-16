8f2a8d8a0f4275fdbc530534832c607fc523ffb1
architgoel007 <architgoel9596@gmail.com>
2021-11-16 12:30:07 +0530

    Add project files.

---
+++ grid/Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using grid.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace grid.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ProductController(ApplicationContext context)
+        {
+            Context = context;
+        }
+
+        public ApplicationContext Context { get; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Category()
+        {
+            return View();
+        }
+
+        public IActionResult SubCategory()
+        {
+            return View();
+        }
+
+        public IActionResult GetProducts()
+        {
+            var products = Context.Products.ToList();
+            return Json(products);
+        }
+
+        public IActionResult CreateProducts(Product product)
+        {
+            Context.Products.ToList();
+            Context.SaveChanges();
+            return Json(product);
+        }
+
+
+    }
+}
---
+++ grid/Models/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace grid.Models
+{
+    public class ApplicationContext:DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext>options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
+    }
+}
